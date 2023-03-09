964b2bdfbb0b6fa7847f7674f757ffe9535c5be6
Vaclav Stencl <stencl@atteq.com>
2023-03-09 19:13:11 +0100

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-  target: 'static',
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
@@ -11,14 +8,8 @@ export default defineNuxtConfig({
     '@nuxt/image-edge',
     '@nuxtjs/tailwindcss',
   ],
-  buildModules: [
-    '@nuxtjs/eslint-module',
-  ],
   image: {
     dir: 'static/images',
-  },
-  static: {
-    prefix: 'public',
   },
   css: [
     '~/assets/scss/main.scss',
