12213b16ccc131334dd6ee197982c06ea91e05ef
Vaclav Stencl <stencl@atteq.com>
2023-04-01 22:52:28 +0200

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,9 @@
-import {createWebHistory} from 'vue-router'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
   app: {
     pageTransition: {name: 'page', mode: 'out-in'},
     baseURL: "/jestedska-odysea-web/",
-  },
-  router: {
-    base: "/jestedska-odysea-web/",
-    options: {
-      history: createWebHistory('/jestedska-odysea-web/'),
-    },
   },
   modules: [
     '@nuxt/image-edge',
